a6cc2841f6aec2c113dd6444cb4141f9f88a9c8a
知念英弥 <s19013@std.it-college.ac.jp>
2022-04-13 09:55:28 +0900

    staff_top.phpを作成

---
+++ staff_login/staff_top.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ショップ管理トップメニュー</h1>
+    <a href="../staff/staff_list.php">スタッフ管理</a><br>
+    <a href="../product/pro_list.php">商品管理</a><br>
+    <a href="../staff_login/staff_top.php">トップメニューへ</a><br>
+</body>
+</html>
