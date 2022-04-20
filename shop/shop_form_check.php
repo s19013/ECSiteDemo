f7a581aa64384c767a3a80feaee5189b150dc5e6
知念英弥 <s19013@std.it-college.ac.jp>
2022-04-20 18:05:57 +0900

    会員登録の情報を受取る,shop_form_doneに渡す

--- shop/shop_form_check.php
+++ shop/shop_form_check.php
@@ -20,11 +20,31 @@
         $postal2 = $post['postal2'];
         $address = $post['address'];
         $tel =$post['tel'];
+
+        $order  =$post['order'];
+        $pass   =$post['pass'];
+        $pass2  =$post['pass2'];
+        $gender =$post['gender'];
+        $birth  =$post['birth'];
         // メール確認
         if (preg_match('/\A[\w\-\.]+\@[\w\-\.]+\.([a-z]+)\z/',$email)==0) {
             echo"<p>メールを正確に入力してください</p>";
             $okFlag=false;
         }
+
+        //会員登録情報
+        if ($order="order_join_member") {
+            if ($pass!=$pass2) {
+                echo "<p>パスワードが一致しません</p>";
+                $okFlag=false;
+            }
+            echo "<p>性別:";
+            if ($gender = 'male') {echo "男性</p>";}
+            else {echo "女性</p>";}
+            echo "<p>生まれ年:{$birth}年代</p>";
+        }
+
+        //個人情報
         if ($okFlag=true) {
             echo <<<EOM
             <p>お名前:{$name}</p>
@@ -35,18 +55,22 @@
             EOM;
             echo <<<EOM
             <form action="shop_form_done.php" method="post">
-            <input type="hidden" name="name"    value=$name>
-            <input type="hidden" name="email"   value=$email>
-            <input type="hidden" name="postal1" value=$postal1>
-            <input type="hidden" name="postal2" value=$postal2>
-            <input type="hidden" name="address" value=$address>
-            <input type="hidden" name="tel"     value=$tel>
-            <input type="button" value="戻る" onclick="history.back()">
-            <input type="submit" value="次へ">
+                <input type="hidden" name="name"    value=$name>
+                <input type="hidden" name="email"   value=$email>
+                <input type="hidden" name="postal1" value=$postal1>
+                <input type="hidden" name="postal2" value=$postal2>
+                <input type="hidden" name="address" value=$address>
+                <input type="hidden" name="tel"     value=$tel>
+                <input type="hidden" name="order"   value=$order>
+                <input type="hidden" name="pass"    value=$pass>
+                <input type="hidden" name="gender"  value=$gender>
+                <input type="hidden" name="birth"   value=$birth>
+                <input type="button" value="戻る" onclick="history.back()">
+                <input type="submit" value="次へ">
             </form>
             EOM;
         }
+
     ?>
-    
 </body>
 </html>
