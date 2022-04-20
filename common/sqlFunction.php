377bd6b2edfc99b2fd641e3569a2cda62cf140fe
知念英弥 <s19013@std.it-college.ac.jp>
2022-04-20 11:51:11 +0900

    一応変数､関数の用意はした｡ただ実装は後

--- common/sqlFunction.php
+++ common/sqlFunction.php
@@ -1,3 +1,30 @@
 <?php
-    
+    //とりあえずこれで全部初期化できたはず
+    $dsn  =null;
+    $user =null;
+    $password='';
+    $dbh = null;
+    $stmt = null;
+
+    function connectDB()
+    {
+        $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+        $user = 'root';
+        $dbh = new PDO($dsn,$user,$password);
+        $dbh -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    }
+
+    function actSql($sql = null)
+    {
+        $stmt = $dbh ->prepare($sql);
+
+    }
+
+    function disconnectDB()
+    {
+        $dsn  =null;
+        $dbh = null;
+        $user =null;
+        $stmt = null;
+    }
 ?>
