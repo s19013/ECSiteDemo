4732e1f538dd5dae303279a44bdea7fa8b38b451
知念英弥 <s19013@std.it-college.ac.jp>
2022-04-20 18:48:59 +0900

    ログインフォーム作成

---
+++ member/member_login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>メールアドレス</p>
+    <input type="text" name="email" id="">
+    <p>パスワード</p>
+    <input type="password" name="pass" id="">
+    <input type="submit" value="ログイン">
+</body>
+</html>
