070a76350f76b117b5c926f4bdbd0a56a9c0b771
知念英弥 <s19013@std.it-college.ac.jp>
2022-04-23 18:15:04 +0900

    商品をdbから取ってきて表示するのを関数化

--- shop/shop_list.php
+++ shop/shop_list.php
@@ -1,12 +1,40 @@
 <?php
+    require_once("../common/DB.php");
     $myPageLinkOrSuggestLogin=null;
     session_start();
     session_regenerate_id(true);
     if (isset($_SESSION['member_login'])==false) {
-        
         $myPageLinkOrSuggestLogin="<a href=../member/member_login.php>ログイン or 会員登録</a>";
     } else {
-        $myPageLinkOrSuggestLogin="<p onclick=location.href=../member/member_page.php>マイページ</p>";
+        $myPageLinkOrSuggestLogin="<p onclick=location.href='../member/member_page.php'>マイページ</p>";
+    }
+
+    function showProduct(){
+        try {
+            //データベース空データを取って来る
+            $DB = new DB();
+            $DB->connectDB();
+            $sql = 'SELECT * FROM mst_product WHERE 1';
+            $DB->actSql($sql,null);
+            //切断
+            $DB->disconnectDB();
+            while (true) {
+                $rec = $DB->getResult();
+                if ($rec == false) {break;}
+                $pro_img_name= $rec['img'];
+                echo <<< EOM
+                <div class="product" onclick=location.href='shop_product.php?procode={$rec['code']}'>
+                    <p>{$rec['name']}</p>
+                    <img src=../img/yasai/{$pro_img_name}>
+                    <p>{$rec['price']}円</p>
+                </div>
+                EOM;
+            }
+        } catch (Exception $e) {
+            echo $e;
+            echo 'ただいま障害によりご迷惑をおかけしています｡';
+            exit();
+        }
     }
 ?>
 <!DOCTYPE html>
@@ -16,46 +44,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="shoplist.css">
+    <!-- css -->
+    <link rel="stylesheet" href="../scss/shop_css/shop_list.css">
+    <link rel="stylesheet" href="../scss/shop_css/shop_header.css">
+
+    <!-- google webフォント -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- mochipopFont -->
+    <link href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
-        <p>ろくまる農園</p>
-        <?php echo $myPageLinkOrSuggestLogin; ?>
-        <button onclick="location.href='./shop_cartlook.php'"><?php?>カート</button>
+    <p>ろくまる農園</p>
+    <?php echo $myPageLinkOrSuggestLogin; ?>
+    <button onclick="location.href='./shop_cartlook.php'"><?php?>カート</button>
     </header>
-    <?php
-        try {
-            $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
-            $user = 'root';
-            $password = '';
-            $dbh = new PDO($dsn,$user,$password);
-            $dbh -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    <h1>商品一覧</h1>
+    <div class="proList">
+        <?php showProduct()?>
+    </div>
+    
 
-            $sql = 'SELECT * FROM mst_product WHERE 1';
-            $stmt = $dbh -> prepare($sql);
-            $stmt -> execute();
-
-            $dbh = null;
-
-            echo "<p>商品一覧</p>";
-            while (true) {
-                $rec = $stmt -> fetch(PDO::FETCH_ASSOC);
-                if ($rec == false) {break;}
-                echo <<< EOM
-                <a href=shop_product.php?procode={$rec['code']}>
-                {$rec['name']}:{$rec['price']}円
-                </a><br/>
-                EOM;
-            }
-            echo "<a href='shop_cartlook.php'>カートを見る</a>";
-        } catch (Exception $e) {
-            echo 'ただいま障害によりご迷惑をおかけしています｡';
-            exit();
-        }
-
-    ?>
-    <!-- <a href="../staff_login/staff_top.php">トップメニューへ</a><br> -->
-
+    
 </body>
 </html>
