b9b0dc3050e9c00377b935e604e26c7df55074b6
知念英弥 <s19013@std.it-college.ac.jp>
2022-04-23 15:18:31 +0900

    名前を変更して､クラス化した

---
+++ common/DB.php
@@ -0,0 +1,42 @@
+<?php
+    class DB
+    {
+        function __construct() {
+            $this->dsn  =null;
+            $this->user =null;
+            $this->password='';
+            $this->dbh = null;
+            $this->stmt = null;
+        }
+
+        public function connectDB()
+        {
+            $this->dsn  = 'mysql:dbname=shop;host=localhost;charset=utf8';
+            $this->user = 'root';
+            $this->dbh  = new PDO($this->dsn,$this->user,$this->password);
+            $this->dbh -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        }
+
+        public function disconnectDB()
+        {
+            $this->dsn  =null;
+            $this->dbh  =null;
+            $this->user =null;
+            $this->stmt =null;
+        }
+
+        public function actSql($sql,$data)
+        {
+            $this->stmt = $this->dbh -> prepare($sql);
+            $this->stmt -> execute();
+            if (is_null($data)) {$this->stmt -> execute();}
+            else{$this->$stmt -> execute($data);}
+        }
+
+        public function getResult()
+        {
+            return $this->stmt->fetch(PDO::FETCH_ASSOC);
+        }
+    }
+
+?>
